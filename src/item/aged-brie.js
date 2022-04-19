45867a99853175bdb6e3a3f8a22cc0914371d541
Kaxuo <Kaxuo@hotmail.com>
2022-04-19 02:28:46 +0200

    remove unnecessary lines

--- src/item/aged-brie.js
+++ src/item/aged-brie.js
@@ -2,12 +2,9 @@ const Item = require('./item');
 const { RangeMinMax } = require('../helpers');
 
 class AgedBries extends Item {
-  updateQuality() {
-    return this.sellIn < 0 ? this.quality + 2 : this.quality + 1;
-  }
   update() {
     this.sellIn -= 1;
-    this.quality = RangeMinMax(this.updateQuality());
+    this.quality = RangeMinMax(this.sellIn < 0 ? this.quality + 2 : this.quality + 1);
   }
 }
 module.exports = AgedBries;
--- src/item/conjured.js
+++ src/item/conjured.js
@@ -2,12 +2,9 @@ const Item = require('./item');
 const { RangeMinMax } = require('../helpers');
 
 class Conjured extends Item {
-  updateQuality() {
-    return this.sellIn < 0 ? this.quality - 4 : this.quality - 2;
-  }
   update() {
     this.sellIn -= 1;
-    this.quality = RangeMinMax(this.updateQuality());
+    this.quality = RangeMinMax(this.sellIn < 0 ? this.quality - 4 : this.quality - 2);
   }
 }
 module.exports = Conjured;
